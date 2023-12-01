bd696eb826e88607b9a3a07ad40ae7ba3bf83c54
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-30 23:12:28 -0500

    App function finalized

--- src/Components /App/App.js
+++ src/Components /App/App.js
@@ -5,12 +5,12 @@ import Movies from '../Movies/Movies.js'
   
   function App(){
      
-      const [movies, setMovies] = useState(movieData)
+      const [movies, setMovies] = useState(movieData.movies)
       
       return(
           <main className='App'>
           <h1>Rancid-Tomatillos</h1>
-          <p>Test</p>
+          <p>Movie Time!</p>
           <Movies movies = {movies}/>
       </main>
     )
