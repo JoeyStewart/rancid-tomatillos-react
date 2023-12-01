022f8b2ed92cf8124bc4e8401192798b433a3cde
Joseph Stewart <JosephStewart1752@gmail.com>
2023-12-01 09:58:35 -0500

    Poster sizing

--- src/Components /Movies/Movies.js
+++ src/Components /Movies/Movies.js
@@ -1,22 +1,18 @@
 import Card from "../Card/Card.js"
-import movieData from "../../Movies-sample.js"
 import './Movies.css'
-//Do not need {movies} currently, but Lex advised to keep it
-export default function Movies({movies}){
 
-return (
-    <div className="movie-display">
-      {movieData.movies.map((movie) => {
-        return (
+export default function Movies({ movies }) {
+  return (
+    <div className="movies-container">
+      {movies.map((movie) => (
         <Card 
-        title={movie.title} 
-        image={movie.poster_path} 
-        id={movie.id}
-        key={movie.id} 
+            title={movie.title} 
+            image={movie.poster_path} 
+            id={movie.id}
+            key={movie.id} 
         />
-            )}
-        )}
-        </div>
-    );
+      ))}
+    </div>
+  );
 }
 
