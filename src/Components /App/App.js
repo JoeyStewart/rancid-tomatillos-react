c8bf3b3e4775ef661af987b37ecf09d92eb95d24
Deanna Stevens <deanna.sofias@gmail.com>
2023-12-01 17:54:27 -0500

    Add chooseMovie fn

--- src/Components /App/App.js
+++ src/Components /App/App.js
@@ -3,17 +3,32 @@ import movieData from '../../Movies-sample.js'
 import Movies from '../Movies/Movies.js'
 
   
-  function App(){
-     
-      const [movies, setMovies] = useState(movieData.movies)
-      
-      return(
-          <main className='App'>
-          <h1>Rancid-Tomatillos</h1>
-          <p>Movie Time!</p>
-          <Movies movies = {movies}/>
-      </main>
+function App(){
+  const [movies, setMovies] = useState(movieData.movies)
+
+  function chooseMovie(movies, id){
+    console.log(id)
+    let chosenMovie = movies.find( movie => movie.id === id)
+    console.log(chosenMovie)
+    setMovies(chosenMovie) 
+    return(
+      <main className='App'>
+      <h1>Rancid-Tomatillos</h1>
+      <p>Movie Time!</p>
+      <Movies className='movieView' movies = {chosenMovie}/>
+    </main>
     )
   }
+
+
+      
+  return(
+    <main className='App'>
+      <h1>Rancid-Tomatillos</h1>
+      <p>Movie Time!</p>
+      <Movies movies = {movies}/>
+    </main>
+  )
+}
 
 export default App;
