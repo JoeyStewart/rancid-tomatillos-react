3565e5623319c7d3a7d09c153ffcf6625cfb3d20
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-30 23:13:29 -0500

    Display Movies

--- src/Components /Movies/Movies.js
+++ src/Components /Movies/Movies.js
@@ -1,6 +1,22 @@
+import Card from "../Card/Card.js"
+import movieData from "../../Movies-sample.js"
+import './Movies.css'
+//Do not need {movies} currently, but Lex advised to keep it
 export default function Movies({movies}){
-    console.log(movies)
-    // return(
 
-    // )
+return (
+    <div className="movie-display">
+      {movieData.movies.map((movie) => {
+        return (
+        <Card 
+        title={movie.title} 
+        image={movie.poster_path} 
+        id={movie.id}
+        key={movie.id} 
+        />
+            )}
+        )}
+        </div>
+    );
 }
+
