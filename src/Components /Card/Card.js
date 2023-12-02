d3b039f47b3dc4317d504cf8b74517932ac1c2d1
Joseph Stewart <JosephStewart1752@gmail.com>
2023-12-02 13:28:28 -0500

    add info and fade to card view

--- src/Components /Card/Card.js
+++ src/Components /Card/Card.js
@@ -11,7 +11,7 @@ import { useInView } from 'react-intersection-observer';
 //     )
 //   } 
 
-export default function Card({ title, image, id, showDetails }) {
+export default function Card({ title, image, id, showDetails, rating }) {
   const [ref, inView] = useInView({
     triggerOnce: true, 
     threshold: 0.2, 
@@ -25,7 +25,10 @@ export default function Card({ title, image, id, showDetails }) {
       onClick={() => showDetails(id)}
     >
       <img className='poster' src={image} alt={title} />
-      <h3 className='title'>{title}</h3>
+      <div className='movie-info'>
+        <h3 className='title'>{title}</h3>
+        <h3 className='rating'>Rating: {rating}</h3>
+      </div>
     </main>
   );
 }
