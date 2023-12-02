f9bbce2a24e85b53ddf0bbb9bb3222da4850e400
William Lavery <lavery_william@yahoo.com>
2023-12-01 21:16:32 -0500

    Add chosen movie styling

--- src/Components /App/App.js
+++ src/Components /App/App.js
@@ -1,36 +1,43 @@
 import  { useState } from 'react'
 import movieData from '../../Movies-sample.js'
 import Movies from '../Movies/Movies.js'
+import './App.css'
 
-  
 function App(){
+     
   const [movies, setMovies] = useState(movieData.movies)
   const [chosenMovie, setChosenMovie] = useState(null);
-
-  function showDetails(movieId){
-    console.log(movieId)
-    const chosenMovie = movies.find(movie => movie.id === movieId)
-    console.log(chosenMovie)
+  console.log()
+  function showDetails(movieId) {
+    const showMovie = movies.find((movie) => movie.id === movieId);
     setChosenMovie(showMovie);
   }
 
-  return(
+    function backToMain() {
+      setMovies(movieData.movies); 
+      setChosenMovie(null); 
+  }
+
+  return (
     <main className='App'>
-      <h1>Rancid-Tomatillos</h1>
-      <p>Movie Time!</p>
+      <header><h1 style={{ cursor: 'pointer' }} className='rancid'
+        onClick={backToMain}>Rancid-Tomatillos</h1>
+      </header>
       {chosenMovie ? (
-        <main>
-          <h2 title={chosenMovie.title}>Title: {chosenMovie.title}</h2>
-          <h2 title={chosenMovie.average_rating}>Rating: {chosenMovie.average_rating}</h2>
-          <h2 title={chosenMovie.release_date}>Release: {chosenMovie.release_date}</h2>
-           <img src={chosenMovie.backdrop_path}></img>
+        <main className='selectedView'>
+          <h2 className='selectedTitle' title={chosenMovie.title}>Title: {chosenMovie.title}</h2>
+          <h2 className='selectedRating' title={chosenMovie.average_rating}>Rating: {chosenMovie.average_rating}</h2>
+          <h2 className='selectedRelease' title={chosenMovie.release_date}>Release: {chosenMovie.release_date}</h2>
+          <img className='selectedBackdrop' src={chosenMovie.backdrop_path}></img>
         </main>
-           
+        
       ) : (
-            <Movies movies={movies} showDetails={showDetails} />
-          )}
+        <Movies movies={movies} showDetails={showDetails} />
+      )}
     </main>
   );
+
 }
 
 export default App;
+
--- src/Components /Card/Card.js
+++ src/Components /Card/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 export default function Card({ title, image, id, showDetails }){
     console.log(image)
     return (
-      <button className='card'onClick={() => {showDetails(id)}}>
+      <button style={{ cursor: 'pointer' }} className='card' onClick={() => showDetails(id)}>
         <img className='poster' src={image} alt={title}></img>
         <h3 className='title'>{title}</h3>
       </button>
