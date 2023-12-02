44032a92844712e44d47e1bd1075965e445c8041
Joseph Stewart <JosephStewart1752@gmail.com>
2023-12-02 13:29:06 -0500

    Format style and font display

--- src/Components /Movies/Movies.js
+++ src/Components /Movies/Movies.js
@@ -8,6 +8,7 @@ export default function Movies({ movies, showDetails }) {
         <Card 
             title={movie.title} 
             image={movie.poster_path} 
+            rating={movie.average_rating.toFixed(1)}
             id={movie.id}
             key={movie.id} 
             showDetails={() => showDetails(movie.id)}
