1df8ab857cb4931cf8eac19b4ef1fe3143a4c745
Joseph Stewart <JosephStewart1752@gmail.com>
2023-12-01 21:31:09 -0500

    Animation and click created

--- src/Components /Card/Card.js
+++ src/Components /Card/Card.js
@@ -1,11 +1,31 @@
 import './Card.css'
+import { useInView } from 'react-intersection-observer';
 
-export default function Card({ title, image, id }){
-    console.log(image)
-    return (
-      <div className='card'>
-        <img className='poster' src={image}></img>
-        <h3 className='title'>{title}</h3>
-    </div>
-    )
-  } 
+// export default function Card({ title, image, id, showDetails}){
+//     console.log(image)
+//     return (
+//     <main style={{ cursor: 'pointer' }} className='card' onClick={() => showDetails(id)}>
+//       <img className='poster' src={image}/>
+//       <h3 className='title'>{title}</h3>
+//     </main>
+//     )
+//   } 
+
+export default function Card({ title, image, id, showDetails }) {
+  const [ref, inView] = useInView({
+    triggerOnce: true, 
+    threshold: 0.2, 
+  });
+
+  return (
+    <main
+      ref={ref}
+      style={{ cursor: 'pointer' }}
+      className={`card ${inView ? 'fade-in' : ''}`}
+      onClick={() => showDetails(id)}
+    >
+      <img className='poster' src={image} alt={title} />
+      <h3 className='title'>{title}</h3>
+    </main>
+  );
+}
