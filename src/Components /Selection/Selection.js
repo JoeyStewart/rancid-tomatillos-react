465be9753fb56412a9b2c1483cbd86b0dfcc7f32
Joseph Stewart <JosephStewart1752@gmail.com>
2023-12-09 13:18:22 -0500

    Add containers to text for styling

--- src/Components /Selection/Selection.js
+++ src/Components /Selection/Selection.js
@@ -12,21 +12,25 @@ export default function Selection({chosenMovie}) {
         <div className='display-poster'>
           <img className='movie-poster'src={chosenMovie.poster_path} alt={chosenMovie.poster_path}></img>
           <h4 className='selectedRelease' title={chosenMovie.release_date}>({chosenMovie.release_date.slice(0,4)})</h4>
+          {/* <h5 className='selectedGenres'>{chosenMovie.genres}</h5> */}
         </div>
         <div className="info-blurb">
           <h2 className='selectedTitle' title={chosenMovie.title}>{chosenMovie.title}</h2>
           <h4 className='tagline'>{chosenMovie.tagline}</h4>
           <h3 className='selectedOverview' title={chosenMovie.overview}>{chosenMovie.overview}</h3>
           <h3 className='selectedRating' title={chosenMovie.average_rating}>{chosenMovie.average_rating.toFixed(1)}/10 Stars</h3>
-          {/* <h5 className='selectedGenres'>{chosenMovie.genres}</h5> */}
         </div>
       </div>
     </div>
     <div className='res-overlay-mid'>
         <div className='display-poster-mid'>
-          <h2 className='selectedTitle-mid' title={chosenMovie.title}>{chosenMovie.title}</h2>
-          <h4 className='tagline-mid'>{chosenMovie.tagline}</h4>
-          <img className='movie-poster-mid'src={chosenMovie.poster_path} alt={chosenMovie.poster_path}></img>
+          <div className='first-info-mid'>
+            <h2 className='selectedTitle-mid' title={chosenMovie.title}>{chosenMovie.title}</h2>
+            <h4 className='tagline-mid'>{chosenMovie.tagline}</h4>
+          </div>
+          <div className="mid-poster-container">
+            <img className='movie-poster-mid'src={chosenMovie.poster_path} alt={chosenMovie.poster_path}></img>
+          </div>
           <h4 className='selectedRelease-mid' title={chosenMovie.release_date}>({chosenMovie.release_date.slice(0,4)})</h4>
           <h3 className='selectedOverview-mid' title={chosenMovie.overview}>{chosenMovie.overview}</h3>
           <h3 className='selectedRating-mid' title={chosenMovie.average_rating}>{chosenMovie.average_rating.toFixed(1)}/10 Stars</h3>
