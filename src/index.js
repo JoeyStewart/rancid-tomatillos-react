28bfeb4d83a65a90fa786e571266c0cb7ec6e614
Deanna Stevens <deanna.sofias@gmail.com>
2023-12-09 22:21:56 -0500

    Add createHashRouter fn with new root render

--- src/index.js
+++ src/index.js
@@ -4,18 +4,36 @@ import './index.css';
 import App from '../src/Components /App/App.js';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Movies />,
+        loader: moviesLoader,
+      },
+      {
+        path: "movie/:movieId",
+        element: <Selection />,
+        loader: selectionLoader,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<BrowserRouter>
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-  </BrowserRouter>
 );
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
