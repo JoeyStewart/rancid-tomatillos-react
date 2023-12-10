344c17bd66d6d637cd3eafe88865a37ef8606aa4
Deanna Stevens <deanna.sofias@gmail.com>
2023-12-09 23:23:08 -0500

    Add Link to return and imports

--- src/Components /Card/Card.js
+++ src/Components /Card/Card.js
@@ -1,30 +1,30 @@
-import './Card.css'
-import { useInView } from 'react-intersection-observer';
-import PropTypes from 'prop-types'
+import "./Card.css";
+import { useInView } from "react-intersection-observer";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export default function Card({ title, image, id, showDetails, rating }) {
+export default function Card({ title, image, id, rating }) {
   const [ref, inView] = useInView({
-    triggerOnce: true, 
-    threshold: 0.2, 
+    triggerOnce: true,
+    threshold: 0.2,
   });
 
   return (
-    <main to={`/${id}`} className='card-link'>
+    <Link to={`movie/${id}`}>
       <section
         ref={ref}
-        style={{ cursor: 'pointer' }}
-        className={`card ${inView ? 'fade-in' : ''}`}
-        onClick={() => showDetails(id)}
+        style={{ cursor: "pointer" }}
+        className={`card ${inView ? "fade-in" : ""}`}
       >
-        <img className='poster' src={image} alt={title} />
-        <div className='movie-info'>
-          <h3 className='title'>{title}</h3>
-          <h3 className='rating'>{rating}/10 Stars</h3>
+        <img className="poster" src={image} alt={title} />
+        <div className="movie-info">
+          <h3 className="title">{title}</h3>
+          <h3 className="rating">{rating}/10 Stars</h3>
         </div>
       </section>
-    </main>
+    </Link>
   );
-};
+}
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
@@ -32,6 +32,4 @@ Card.propTypes = {
   id: PropTypes.number.isRequired,
   showDetails: PropTypes.func.isRequired,
   rating: PropTypes.string.isRequired,
-
- };
-
+};
