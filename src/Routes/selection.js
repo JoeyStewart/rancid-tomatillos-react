7cd371e82d6711debb45558994447d264f84a276
Deanna Stevens <deanna.sofias@gmail.com>
2023-12-09 23:00:35 -0500

    Clean up fetch call and Selection fn

--- src/Routes/selection.js
+++ src/Routes/selection.js
@@ -1,87 +1,26 @@
-import React from 'react'
 // import PropTypes from 'prop-types'
-// import './Selection.css'
 import { useLoaderData } from "react-router-dom";
-
+import Movie from "../Components/Movie/Movie";
 
 export async function loader({ params }) {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${params.movieId}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Error code: ${response.status}`);
-        }
-        return response.json();
-      })
+  return fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${params.movieId}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Error code: ${response.status}`);
+    }
+    return response.json();
+  });
 }
 
 function Selection() {
   const data = useLoaderData();
   const chosenMovie = data.movie;
 
-  return (
-  <section className='selectedView'>
-    <div className="backdrop">
-      <img className='selectedBackdrop' src={chosenMovie.backdrop_path} alt={chosenMovie.title}></img>
-      <div className="overlay-content">
-        <div className='display-poster'>
-          <img className='movie-poster'src={chosenMovie.poster_path} alt={chosenMovie.poster_path}></img>
-          <h4 className='selectedRelease' title={chosenMovie.release_date}>({chosenMovie.release_date.slice(0,4)})</h4>
-        </div>
-        <div className="info-blurb">
-          <h2 className='selectedTitle' title={chosenMovie.title}>{chosenMovie.title}</h2>
-          <h4 className='tagline'>{chosenMovie.tagline}</h4>
-          <h3 className='selectedOverview' title={chosenMovie.overview}>{chosenMovie.overview}</h3>
-          <h3 className='selectedRating' title={chosenMovie.average_rating}>{chosenMovie.average_rating.toFixed(1)}/10 Stars</h3>
-        </div>
-      </div>
-    </div>
-    <div className='res-overlay-mid'>
-        <div className='display-poster-mid'>
-          <div className='first-info-mid'>
-            <h2 className='selectedTitle-mid' title={chosenMovie.title}>{chosenMovie.title}</h2>
-            <h4 className='tagline-mid'>{chosenMovie.tagline}</h4>
-          </div>
-          <div className="mid-poster-container">
-            <img className='movie-poster-mid'src={chosenMovie.poster_path} alt={chosenMovie.poster_path}></img>
-            <h4 className='selectedRelease-mid' title={chosenMovie.release_date}>({chosenMovie.release_date.slice(0,4)})</h4>
-          </div>
-          <div className='second-info-mid'>
-            <h3 className='selectedOverview-mid' title={chosenMovie.overview}>{chosenMovie.overview}</h3>
-            <h3 className='selectedRating-mid' title={chosenMovie.average_rating}>{chosenMovie.average_rating.toFixed(1)}/10 Stars</h3>
-            <h5 className='selectedGenres-mid'>{chosenMovie.genres}</h5>
-          </div>
-        </div>
-      </div>
-      <div className='res-overlay-small'>
-        <div className='display-poster-small'>
-          <div className='first-info-small'>
-            <h2 className='selectedTitle-small' title={chosenMovie.title}>{chosenMovie.title}</h2>
-            <h4 className='tagline-small'>{chosenMovie.tagline}</h4>
-          </div>
-          <img className='movie-poster-small'src={chosenMovie.poster_path} alt={chosenMovie.poster_path}></img>
-          <h4 className='selectedRelease-small' title={chosenMovie.release_date}>({chosenMovie.release_date.slice(0,4)})</h4>
-          <div className='second-info-small'>
-            <h3 className='selectedOverview-small' title={chosenMovie.overview}>{chosenMovie.overview}</h3>
-            <h3 className='selectedRating-small' title={chosenMovie.average_rating}>{chosenMovie.average_rating.toFixed(1)}/10 Stars</h3>
-            <h5 className='selectedGenres-small'>{chosenMovie.genres}</h5>
-          </div>
-        </div>
-      </div>
-  </section>
-  )
+  if (!chosenMovie) {
+    return <p>No Movie</p>;
+  }
+  return <Movie chosenMovie={chosenMovie} />;
 }
-
-// Selection.propTypes = {
-//   chosenMovie: PropTypes.shape({
-//     title: PropTypes.string.isRequired,
-//     backdrop_path: PropTypes.string.isRequired,
-//     poster_path: PropTypes.string.isRequired,
-//     release_date: PropTypes.string.isRequired,
-//     tagline: PropTypes.string.isRequired,
-//     overview: PropTypes.string.isRequired,
-//     average_rating: PropTypes.number.isRequired
-//   }).isRequired
-//  };
-
 
  export default Selection
