8282bf40c684d2790fd6004084647dc113296733
Deanna Stevens <deanna.sofias@gmail.com>
2023-12-09 23:26:04 -0500

    Refactor component w loader async fetch call

--- src/Components /Movies/Movies.js
+++ src/Components /Movies/Movies.js
@@ -1,39 +1,55 @@
-import Card from "../Card/Card.js"
-import './Movies.css'
-import PropTypes from 'prop-types'
+import Card from "../Card/Card.js";
+import "./Movies.css";
+import { useLoaderData } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export default function Movies({ movies, showDetails }) {
-  if (!movies || movies.length === 0) {
-    return null;
-  } else {
-    return (
-      <div className="movies-container">
-      {movies.map((movie) => (
-        <Card 
-        title={movie.title} 
-        image={movie.poster_path} 
-        rating={movie.average_rating.toFixed(1)}
-        id={movie.id}
-        key={movie.id} 
-        showDetails={() => showDetails(movie.id)}
-        />
-        ))}
-    </div>
+export async function loader() {
+  const data = await fetch(
+    "https://rancid-tomatillos.herokuapp.com/api/v2/movies"
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response);
+    }
+    return response.json();
+  });
+  return { data };
+}
+
+function Movies() {
+  const { data } = useLoaderData();
+  const movies = data.movies;
+
+  return (
+    <>
+      {movies.length ? (
+        <div className="movies-container">
+          {movies.map((movie) => (
+            <Card
+              title={movie.title}
+              image={movie.poster_path}
+              rating={movie.average_rating.toFixed(1)}
+              id={movie.id}
+              key={movie.id}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>No Movies</p>
+      )}
+    </>
   );
 }
-}
-
 
 Movies.propTypes = {
   movies: PropTypes.arrayOf(
-   PropTypes.shape({
-     title: PropTypes.string,
-     poster_path: PropTypes.string,
-     average_rating: PropTypes.number,
-     id: PropTypes.number,
-   })
+    PropTypes.shape({
+      title: PropTypes.string,
+      poster_path: PropTypes.string,
+      average_rating: PropTypes.number,
+      id: PropTypes.number,
+    })
   ).isRequired,
   showDetails: PropTypes.func.isRequired,
-
 };
 
+export default Movies;
