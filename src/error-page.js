91fa1f423153705d5c1d2adde68486fe7da723a2
William Lavery <lavery_william@yahoo.com>
2023-12-10 17:55:47 -0500

    Add error-handling

--- src/error-page.js
+++ src/error-page.js
@@ -4,7 +4,7 @@ export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div>
+    <div className=".error-message">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
