8c52c94e52aa9ccc4eb6e468c500666ca0d157d2
William Lavery <lavery_william@yahoo.com>
2023-12-10 17:59:29 -0500

    Fix typo

--- src/error-page.js
+++ src/error-page.js
@@ -4,7 +4,7 @@ export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div className=".error-message">
+    <div className="error-message">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
