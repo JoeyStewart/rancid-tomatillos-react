ab6c30b484bb3431ad562320041d7ab99bf42546
William Lavery <lavery_william@yahoo.com>
2023-12-10 22:16:15 -0500

    Update error-page.js

--- src/error-page.js
+++ src/error-page.js
@@ -4,9 +4,8 @@ export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <div className="error-message">
+      <p>Error: Click Title to Navigate Back to Home Page</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
